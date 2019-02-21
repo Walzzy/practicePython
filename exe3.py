ba558e02f0617f6bae176c76938d428753ec03b5
Waleed <waleedal_qasem@hotmail.com>
2019-02-22 00:03:04 +1100

    Updated Code

--- exe3.py
+++ exe3.py
@@ -1,1 +1,36 @@
-print("Hello This is my first Git)
+# Take a list, say for example this one:
+#
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+#
+# Extras:
+#
+# Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+# Write this in one line of Python.
+# Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+def lessThanTen(myList):
+    newList = []
+    for x in myList:
+        if(x<5):
+            newList.append(x)
+    return newList
+
+
+def part2(myList):
+    return [x for x in myList if(x<5)]
+
+def part3(myList, myNum):
+    return [x for x in myList if(x<myNum)]
+
+
+
+
+
+
+lst1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+print(lessThanTen(lst1))
+
+print(part2(lst1))
+print(part3(lst1, 56))
