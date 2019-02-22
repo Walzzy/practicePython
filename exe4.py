d63811a658ed78a11a0ab410533afd6b0e918526
Waleed <waleedal_qasem@hotmail.com>
2019-02-23 01:13:25 +1100

    "Finished Execrise 4"

--- exe4.py
+++ exe4.py
@@ -1,22 +1,20 @@
-# Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+# -*- coding: utf-8 -*-
+## # Create a program that asks the user for a number and then prints out a
+# list of all the divisors #of that number. (If you don’t know what a divisor is,
+# it is a number that divides evenly into #another number. For example, 13 is a
+# divisor of 26 because 26 / 13 has no remainder.)
+
 
 def divisors(x):
     numbers = range(x,0,-1)
+    divisorList = []
 
     for y in numbers:
-        if (x%y == 0):
-            return y
+        if(x%y == 0):
+            divisorList.append(y)
+
+    #divisorList.reverse()
+    return divisorList
 
 
-
-
-
-#print(divisors(10))
-
-
-x = 10
-numbers = range(x,0,-1)
-
-for x in numbers:
-    if(10%x == 0):
-        print(x)
+print(divisors(10))
