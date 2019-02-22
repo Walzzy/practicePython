ea2a9401728b3815693e0c1841cf0b3db7a7d133
Waleed <waleedal_qasem@hotmail.com>
2019-02-23 01:18:16 +1100

    adding exe5 problem statement

--- exe5.py
+++ exe5.py
@@ -1,15 +1,11 @@
-
-__author__ = 'jeffreyhunt'
-
-num = int(input("Please choose a number to divide: "))
-
-listRange = list(range(1,num+1))
-
-divisorList = []
-
-for number in listRange:
-    if num % number == 0:
-        divisorList.append(number)
-
-divisorList.reverse()
-print(divisorList)
+# -*- coding: utf-8 -*-
+# Take two lists, say for example these two:
+#
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+#
+# Extras:
+#
+# Randomly generate two lists to test this
+# Write this in one line of Python (don’t worry if you can’t figure this out at this point  we’ll get to it soon)
