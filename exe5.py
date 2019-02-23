c74b459737af7b4f7d0085af9f57661503ab3150
Waleed <waleedal_qasem@hotmail.com>
2019-02-24 01:48:58 +1100

    Adding exe5 and exe6

--- exe5.py
+++ exe5.py
@@ -9,3 +9,20 @@
 #
 # Randomly generate two lists to test this
 # Write this in one line of Python (don’t worry if you can’t figure this out at this point  we’ll get to it soon)
+
+
+def listChecker(list1, list2):
+    duplicateList = []
+
+    for x in list1:
+        for y in list2:
+            if (x == y) and (x not in duplicateList):
+                duplicateList.append(x)
+
+    return duplicateList
+
+
+
+b = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print(listChecker(a, b))
--- exe6.py
+++ exe6.py
@@ -1,1 +1,22 @@
-print("My name is Medhat")
+#Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+
+def palindrome(myStr):
+
+    newStr = myStr[::-1].lower()
+
+    print(newStr)
+    print(myStr)
+
+    if newStr == myStr.lower():
+        return True
+    else:
+        return False
+
+
+
+
+print(palindrome("Anna"))
+# a = "Waleed"
+# strLen = len(a)
+# b = a[::-1]
+# print(b)
