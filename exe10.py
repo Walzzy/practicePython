aef26a519908a1429b714d62c2973925129cf18a
Waleed <waleedal_qasem@hotmail.com>
2019-03-03 12:44:34 +1100

    exe10.py

---
+++ exe10.py
@@ -0,0 +1,14 @@
+import random
+
+def overLapList(lst1, lst2):
+
+    a = random.sample(range(1,30), 12)
+    b = random.sample(range(1,30), 16)
+    
+    result_overlap = [i for i in set(a) if i in b]
+
+    result = []
+
+    for element in result_overlap:
+      if element not in result:
+        result.appent(element)
